72debd0dfeb1ec31661fe24f585ea97a87e4df78
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-08-01 20:50:38 +0530

    crsr js added

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
+
+function circleMouseFollower() {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+  });
+}
+ circleMouseFollower();
