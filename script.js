bd7218990a801f5594cf9fb183dbb17d97d21187
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-08-02 19:24:50 +0530

    page1 animation added

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
+function firstPageAnim(){
+  var tl = gsap.timeline();
+
+  tl.to(".boundingelem_down , .boundingelem_top", {
+    y: 0,
+    duration: 2,
+    ease: Expo.easeInOut,
+    stagger: .2
+  })
+}
+
 function circleMouseFollower() {
   window.addEventListener("mousemove", function (dets) {
     document.querySelector(
@@ -10,4 +21,5 @@ function circleMouseFollower() {
     ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
   });
 }
- circleMouseFollower();
+circleMouseFollower();
+firstPageAnim();
