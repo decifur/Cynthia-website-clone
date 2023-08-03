a256a5a11a0ba1414458b1310626670199648833
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-08-03 20:05:20 +0530

    added cursor animation (eclipse)

--- script.js
+++ script.js
@@ -3,23 +3,60 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
-function firstPageAnim(){
+function firstPageAnim() {
   var tl = gsap.timeline();
 
   tl.to(".boundingelem_down , .boundingelem_top", {
     y: 0,
     duration: 2,
     ease: Expo.easeInOut,
-    stagger: .2
-  })
+    delay: -0.5,
+    stagger: 0.2,
+  }).from("#page1footer", {
+    opacity: 0,
+    duration: 1.2,
+    delay: -1,
+    ease: Expo.easeInOut,
+  });
 }
 
-function circleMouseFollower() {
+var timeout;
+
+function circleToEclipse() {
+  var xscale = 1;
+  var yscale = 1;
+
+  var xprev = 0;
+  var yprev = 0;
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeout);
+
+    var xdiff = dets.clientX - xprev;
+    var ydiff = dets.clientY - yprev;
+
+    xscale = gsap.utils.clamp(0.8, 1.2, xdiff);
+    yscale = gsap.utils.clamp(0.8, 1.2, ydiff);
+
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+
+    circleMouseFollower(xscale, yscale);
+    timeout = setTimeout(function () {
+      document.querySelector(
+        "#minicircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1,1)`;
+    }, 100);
+  });
+}
+
+function circleMouseFollower(xscale, yscale) {
   window.addEventListener("mousemove", function (dets) {
     document.querySelector(
       "#minicircle"
-    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
   });
 }
+
+circleToEclipse();
 circleMouseFollower();
 firstPageAnim();
